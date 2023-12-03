69821c95b1b2e613fdc3b96eebe2c3f705093161
SHOBINROY <149036070+SHOBINROY@users.noreply.github.com>
2023-12-03 21:00:17 +0530

    Delete travelproject1 directory

--- travelproject1/travelapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Place
-from .models import tips
-
-admin.site.register(Place)
-admin.site.register(tips)
-# Register your models here.
--- travelproject1/travelapp/urls.py
+++
@@ -1,5 +0,0 @@
-from . import views
-from django.urls import path
-urlpatterns = [
-      path('',views.demo,name="demo")
-]
